1a0259ffaccfc076539f4382e6d967e992d3210e
Andrey Verstov <ener@enerdesign.ru>
2011-12-25 18:08:40 +0400

    bug fix generate forms

--- views/core/generator/form.php
+++ views/core/generator/form.php
@@ -21,17 +21,17 @@
 ?>
 <?php switch(get_class($field)) {
 	case 'Field_Text':
-	echo "\t" . '<?php echo Core_Forms::text_area(\'' . $field->name . '\', $'.$model . '->' . $field->name . ', ' . $label . ', $errors, false);?>;' . "\n";
+	echo "\t" . '<?php echo Core_Forms::text_area(\'' . $field->name . '\', $'.$model . '->' . $field->name . ', ' . $label . ', $errors, false);?>' . "\n";
 	break;
 
 	case 'Field_String':
 	case 'Field_Float':
 	case 'Field_Integer':
-		echo "\t" . '<?php echo Core_Forms::text_input(\'' . $field->name . '\', $'.$model . '->' . $field->name . ', ' . $label . ', $errors, false);?>;' . "\n";
+		echo "\t" . '<?php echo Core_Forms::text_input(\'' . $field->name . '\', $'.$model . '->' . $field->name . ', ' . $label . ', $errors, false);?>' . "\n";
 		break;
 
 	case 'Field_Boolean':
-		echo "\t" . '<?php echo Core_Forms::checkbox(\'' . $field->name . '\', $'.$model . '->' . $field->name . ', ' . $label . ', $errors, false);?>;' . "\n";
+		echo "\t" . '<?php echo Core_Forms::checkbox(\'' . $field->name . '\', $'.$model . '->' . $field->name . ', ' . $label . ', $errors, false);?>' . "\n";
 		break;
  } ?>
 <?php endforeach;?>
